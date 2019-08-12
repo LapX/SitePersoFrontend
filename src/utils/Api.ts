5fc2ca13009df5f96f1b4eb647003c46c6275dc2
Montreuil <montreuiltommy@gmail.com>
2019-08-11 22:48:34 -0400

    Removed useless interfaces

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -2,16 +2,6 @@ import axios from 'axios'
 
 const ApiUrl: String = 'https://shrouded-reaches-97153.herokuapp.com'
 
-export interface Datalist {
-    ID: number,
-    Tuples: Item[]
-}
-
-export interface Item {
-    Quarter: number,
-    Earnings: number
-}
-
 export async function getData() {
     return await axios.get(ApiUrl + '/data')
         .then(response => {
