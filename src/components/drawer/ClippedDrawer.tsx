3f5f47c0b2df896c0b908a036473fe2de7fcab51
Montreuil <montreuiltommy@gmail.com>
2019-10-06 20:29:23 -0400

    Added side drawer with graphs box

--- src/components/drawer/ClippedDrawer.tsx
+++ src/components/drawer/ClippedDrawer.tsx
@@ -1,14 +1,23 @@
-import {Drawer, List, ListItem, ListItemText, Theme} from "@material-ui/core";
+import {Drawer, ListItem, ListItemText, Theme} from "@material-ui/core";
 import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import createStyles from "@material-ui/styles/createStyles/createStyles";
 import {ThemeProvider} from "@material-ui/styles";
+import Avatar from "@material-ui/core/Avatar";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChartBar} from "@fortawesome/free-solid-svg-icons";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+
 
 interface IProps {
     theme: Theme
 }
 
-const drawerWidth = 140;
+const drawerWidth = 170;
 
 const style = makeStyles((theme) =>
     createStyles({
@@ -20,6 +29,20 @@ const style = makeStyles((theme) =>
             width: drawerWidth,
             backgroundColor: '#242424',
             color: '#ffffff'
+        },
+        expansionPanel: {
+            backgroundColor: '#242424',
+            color: '#ffffff'
+        },
+        button: {
+            backgroundColor: '#202020',
+            color: '#ffffff',
+            fontSize: 25,
+            width: 60
+        },
+        avatar: {
+            backgroundColor: '#004587',
+            marginRight: 12
         },
         toolbar: theme.mixins.toolbar
     }))
@@ -37,13 +60,24 @@ const ClippedDrawer = (props: IProps) => {
                 paper: classes.drawerPaper,
             }}>
                 <div className={classes.toolbar}/>
-                <List>
-                    {['Graphs', 'Todo list'].map((text, index) => (
-                        <ListItem button key={text}>
-                            <ListItemText primary={text}/>
+                <ExpansionPanel className={classes.expansionPanel}>
+                    <ExpansionPanelSummary
+                    >
+                        <ListItem button key={'Graphs'}>
+                            <Avatar className={classes.avatar}>
+                                <FontAwesomeIcon icon={faChartBar}/>
+                            </Avatar>
+                            <ListItemText primary={'Graphs'}/>
                         </ListItem>
-                    ))}
-                </List>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <ButtonGroup size="small" aria-label="small outlined button group"
+                        >
+                            <Button className={classes.button}>+</Button>
+                            <Button className={classes.button}>-</Button>
+                        </ButtonGroup>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
             </Drawer>
         </ThemeProvider>
     );
